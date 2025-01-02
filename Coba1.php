d9f3d1a2b66d3e38626570f2a093032449110630
GheanAnuno <150656118+GheanAnuno@users.noreply.github.com>
2025-01-02 11:56:11 +0700

    Add files via upload

---
+++ Coba1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Coba 2</title>
+</head>
+<body>
+<?php
+echo "<B>Selamat Datang, $nama!</B>"; echo "<HR>\n";
+echo "Hari ini tanggal : " . date("d F Y");
+?>
+</body>
+</html>
